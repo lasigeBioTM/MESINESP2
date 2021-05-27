d701eeacd002bea9546e55ab8dad2f97d31556f3
vitokas <vitorandrade.cv@gmail.com>
2021-05-27 13:49:10 +0100

    Create get_data.sh

---
+++ get_data.sh
@@ -0,0 +1,9 @@
+# Download MESINESP2 data
+wget https://zenodo.org/record/4701973/files/Additional%20data.zip?download=1
+unzip Additional\ data.zip?download=1
+
+#Change the directories names
+mv ./Additional\ data/ ./Additional_data/
+mv ./Additional_data/Subtrack1-Scientific_Literature ./Additional_data/Subtrack1
+mv ./Additional_data/Subtrack2-Clinical_Trials ./Additional_data/Subtrack2
+mv ./Additional_data/Subtrack3-Patents ./Additional_data/Subtrack3
